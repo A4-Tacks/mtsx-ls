271f26e9bd4cc329588f414bc2005e40c56b401d
A4-Tacks <wdsjxhno1001@163.com>
2026-02-09 19:29:02 +0800

    Basic union syntax define

--- crates/syntax/src/ast.rs
+++ crates/syntax/src/ast.rs
@@ -1,4 +1,4 @@
-use parser::{AstNode, NodeOrToken, SyntaxKind, SyntaxNode, SyntaxToken};
+pub use parser::{AstNode, NodeOrToken, SyntaxKind, SyntaxNode, SyntaxToken};
 
 macro_rules! define_nodes {
     {$(
@@ -13,27 +13,20 @@ macro_rules! define_nodes {
         $(*$rmethod:ident . $rchild:ident),*
         $(,)?
     ]) => {
-        pub struct $name(SyntaxNode);
-
-        impl AstNode for $name {
-            type Language = parser::Language;
-
-            fn cast(node: SyntaxNode) -> Option<Self> {
-                Self::can_cast(node.kind()).then(|| $name(node))
-            }
-
-            fn can_cast(kind: SyntaxKind) -> bool {
-                matches!(kind, $(SyntaxKind::$kinds)|+)
-            }
-
-            fn syntax(&self) -> &SyntaxNode {
-                &self.0
-            }
-        }
+        define_nodes!(@enum $name $(. $kinds)+);
+        define_nodes!(@AstNode $name $(. $kinds)+);
 
         impl $name {
             $(define_nodes!(@impl $name $method $child);)*
             $(define_nodes!(@impl*$name $rmethod $rchild);)*
+        }
+    };
+    (@enum $name:ident . $kinds:ident) => {
+        pub struct $name(SyntaxNode);
+    };
+    (@enum $name:ident $(. $kinds:ident)+) => {
+        pub enum $name {
+            $($kinds($kinds),)*
         }
     };
     (@impl $name:ident $method:ident $child:ident) => {
@@ -46,23 +39,61 @@ macro_rules! define_nodes {
             parser::support::children(&self.0)
         }
     };
+    (@AstNode $name:ident . $kinds:ident) => {
+        impl AstNode for $name {
+            type Language = parser::Language;
+
+            fn cast(node: SyntaxNode) -> Option<Self> {
+                Self::can_cast(node.kind()).then(|| $name(node))
+            }
+
+            fn can_cast(kind: SyntaxKind) -> bool {
+                matches!(kind, SyntaxKind::$kinds)
+            }
+
+            fn syntax(&self) -> &SyntaxNode {
+                &self.0
+            }
+        }
+    };
+    (@AstNode $name:ident $(. $kinds:ident)+) => {
+        impl AstNode for $name {
+            type Language = parser::Language;
+
+            fn cast(node: SyntaxNode) -> Option<Self> {
+                $(if $kinds::can_cast(node.kind()) {
+                    Some(Self::$kinds($kinds(node)))
+                } else)+ {
+                    None
+                }
+            }
+
+            fn can_cast(kind: SyntaxKind) -> bool {
+                $($kinds::can_cast(kind))||+
+            }
+
+            fn syntax(&self) -> &SyntaxNode {
+                match self {
+                    $(Self::$kinds(it) => it.syntax()),+
+                }
+            }
+        }
+    };
+    ($($t:tt)*) => {
+        compile_error!(stringify!($($t)*));
+    };
 }
 
 define_nodes! {
     SourceFile.SOURCE_FILE [table.Table];
     Table.TABLE [*pairs.Pair];
-    Pair.PAIR [
-        join.Join,
-        table.Table,
-        array.Array,
-        literal.Literal,
-        call.Call,
-    ];
+    Pair.PAIR [value.Value];
     Literal.LITERAL [];
     Join.JOIN [];
     Array.ARRAY [*items.Item];
     Item.ITEM [];
     Call.CALL [];
+    Value.Join.Table.Array.Literal.Call [];
 }
 
 pub enum Key {
